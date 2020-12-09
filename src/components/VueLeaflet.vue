15c6be7892a57988176ff8711381c5df7f7e9c80
fengjutian <2640111081@qq.com>
2020-12-09 18:07:37 +0800

    save

--- src/components/VueLeaflet.vue
+++ src/components/VueLeaflet.vue
@@ -1,67 +1,137 @@
 <template>
 <div class='vue-leaflet'>
-  <l-map style="width: 100%; height: 100vh;" :zoom="zoom" :center="center">
+  <!-- <l-map style="width: 100%; height: 100vh;" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
     <l-marker :lat-lng="marker">
       <l-popup :content="text"></l-popup>
     </l-marker>
-  </l-map>
+  </l-map> -->
 
-  <div id='map'></div>
+  <div id='map' style='width:100vw;height:100vh;' ></div>
 
 </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet'
+import proj4 from 'proj4'
+// import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet'
 import L from 'leaflet'
 export default {
   name: 'VueLeaflet',
   components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LPopup
+    // LMap,
+    // LTileLayer,
+    // LMarker,
+    // LPopup
   },
   data () {
     return {
-      zoom: 10,
-      center: L.latLng(76.40,119.93),
-     // layerType: 'vec',
+      // zoom: 10,
+      // center: L.latLng(76.40, 119.93),
+      // // layerType: 'vec',
 
-     //  url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-     url: 'http://t0.tianditu.com/vec_c/wmts?layer=vec&style=default&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=tiles&TileMatrix={z}&TileCol={x}&TileRow={y}&tk=f0c94499ef7f7f653c3aced1e2d5b7f9',
+      // //  url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      // // url: 'http://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={x}&TILECOL={y}&tk=f0c94499ef7f7f653c3aced1e2d5b7f9',
+      // url: 'http://t0.tianditu.com/vec_c/wmts?layer=vec&style=default&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=tiles&TileMatrix={z}&TileCol={x}&TileRow={y}&tk=f0c94499ef7f7f653c3aced1e2d5b7f9',
 
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: L.latLng(76.40,119.93),
-      text: 'this is a marker',
-      zoomOffset: 1
+      // attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      // marker: L.latLng(76.40, 119.93),
+      // text: 'this is a marker',
+      // zoomOffset: 1
+
+      map: null,
+      point: []
     }
   },
-  mounted() {
+  mounted () {
+    this.$nextTick(() => {
+      // this.initMap()
+    //  this.point = [32.045330, 118.789670]
+      this.point = [31.778890, 119.969700]
+      this.initLeaflet()
 
-    // var url='http://t0.tianditu.com/vec_c/wmts?layer=vec&style=default&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=tiles&TileMatrix={z}&TileCol={x}&TileRow={y}&tk=f0c94499ef7f7f653c3aced1e2d5b7f9';
-    // var emap = new L.TileLayer.WMTS( url ,
-    //   {
-    //       tileSize:256,
-    //       layer: 'vec',
-    //       style: "default",
-    //       tilematrixSet: "c",
-    //       format: "tile",
-    //       attribution: "<a href='https://github.com/mylen/leaflet.TileLayer.WMTS'>GitHub</a>&copy; <a href='http://www.ign.fr'>IGN</a>"
-    //   }
-    // );
-    // var map = L.map('map',{crs:L.CRS.EPSG4326,center: {lon:112 , lat:40},zoom: 13})
-    // map.addLayer(emap)
+      console.log(1, this.Mector2Wgs84(118.789670, 32.045330))
+    })
+  },
+  methods: {
+    initMap () {
+      let map = L.map('map', {
+        minZoom: 3,
+        maxZoom: 14,
+        // center: [32.05493, 118.796252],
+        center: [76.45, 118.81],
+        zoom: 12,
+        zoomControl: true,
+        attributionControl: false,
+        crs: L.CRS.EPSG8357
 
+      })
+      window.map = map
+      L.tileLayer(
+        'http://t0.tianditu.com/vec_c/wmts?layer=vec&style=default&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=tiles&TileMatrix={z}&TileCol={x}&TileRow={y}&tk=f0c94499ef7f7f653c3aced1e2d5b7f9'
+      ).addTo(map)
+    },
+    initLeaflet () {
+      // 风场样式引用
+      var esriDarkGreyCanvas = L.tileLayer(
+        'http://t0.tianditu.com/vec_c/wmts?layer=vec&style=default&tilematrixset=c&Service=WMTS&Request=GetTile&Version=1.0.0&Format=tiles&TileMatrix={z}&TileCol={x}&TileRow={y}&tk=f0c94499ef7f7f653c3aced1e2d5b7f9'
+      )
 
+      // 生成风场实例
+      var velocityLayer = L.velocityLayer({
+        displayValues: true,
+        displayOptions: {
+          velocityType: 'GBR Wind',
+          displayPosition: 'bottomleft',
+          displayEmptyString: 'No wind data'
+        },
+        data: require('./wind-global.json'), // 风场数据
+        minVelocity: 0, // Velocity：速率
+        maxVelocity: 10,
+        velocityScale: 0.005,
+        particleMultiplier: 1 / 1200, // 粒子的数量
+        lineWidth: 5, // 粒子的粗细
+        frameRate: 15, // 定义每秒执行的次数
+        colorScale: ['rgb(47,112,47)', 'rgb(47,112,47)', 'rgb(47,112,47)', 'rgb(47,112,47)', 'rgb(47,112,47)']
+      })
 
+      L.CRS.CustomEPSG4326 = L.extend({}, L.CRS.Earth, {
+        code: 'EPSG:4326',
+        projection: L.Projection.LonLat,
+        transformation: new L.Transformation(1 / 180, 1, -1 / 180, 0.5),
+        scale: function (zoom) {
+          return 256 * Math.pow(2, zoom - 1)
+        }
+      })
 
+      // 添加风场样式至地图中
+      this.map = L.map('map', {
+        // center: [76.45, 118.81],
+        center: this.point,
+        // center: [31.56430025788347, 119.61018574979072],
+        crs: L.CRS.CustomEPSG4326,
+        // crs: L.CRS.EPSG3857,
+        //  crs: L.CRS.EPSG4326,
+        zoom: 12,
+        layers: [esriDarkGreyCanvas]
+      })
 
-   }
- }
+      console.log('getCode', this.map)
+      // 风场实例添加到地图上
+      velocityLayer.addTo(this.map)
+    },
+    Mector2Wgs84 (lng, lat) {
+      return proj4(proj4('EPSG:4326'), proj4('EPSG:3857'), [lng, lat])
+    }
+
+  }
+}
 </script>
 
 <style scoped>
+#map {
+　　width:100%;
+　　height:100vh;
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,19 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-import 'leaflet/dist/leaflet.js'
-import './leaflet-tilelayer-wmts-src.js'
 
-import L from 'leaflet'
+import 'leaflet-velocity/dist/leaflet-velocity.css'
+import 'leaflet-velocity/dist/leaflet-velocity'
 
-/* leaflet icon */
-delete L.Icon.Default.prototype._getIconUrl
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-})
+// import 'leaflet/dist/leaflet.js'
+// import './leaflet-tilelayer-wmts-src.js'
+
+// /* leaflet icon */
+// delete L.Icon.Default.prototype._getIconUrl
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+// })
 
 Vue.config.productionTip = false
 
